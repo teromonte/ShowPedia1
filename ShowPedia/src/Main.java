0c41db2089aacce69335eb9d6c86d1b9daa4f030
Daniel de Almeida Joao <d.joao@campus.fct.unl.pt>
2019-05-14 17:48:25 +0100

    ja subi

--- ShowPedia/src/Main.java
+++ ShowPedia/src/Main.java
@@ -9,8 +9,8 @@ import exceptions.All.NotExistShowException;
 
 public class Main {
 
-	//constantes que definem as mensagens
-	
+	// constantes que definem as mensagens
+
 	private static final String SHOW_ADDED = "%s created.\n";
 	private static final String SHOW_ALREADY_EXISTS = "Show already exists!";
 	private static final String GOODBYE = "Bye!";
@@ -39,26 +39,31 @@ public class Main {
 			return Commands.UNKNOWN;
 		}
 	}
+
 	private static void executeCommand(Commands option, Aplication a1, Scanner in) {
 		switch (option) {
-
 		case HELP:
 			processHelp();
 			break;
 		case ADDSHOW:
 			addShow(in, a1);
 			break;
-		case CURRENTSHOW: currentShow(a1);break;
+		case CURRENTSHOW:
+			currentShow(a1);
+			break;
+		case SWITCHTOSHOW:
+			switchToShow(in, a1);
+			break;
 		default:
 			break;
 		}
 	}
 
-	
 	private static void processHelp() {
 		for (StaticMethods.Help d : StaticMethods.Help.values())
 			StaticMethods.pegaAjuda(d);
 	}
+
 	private static void addShow(Scanner in, Aplication a1) {
 		String showName = in.nextLine();
 		try {
@@ -71,16 +76,17 @@ public class Main {
 	private static void currentShow(Aplication a1) {
 		try {
 			System.out.println(a1.getCurrentShow());
-		} catch(NotExistShowException except) {
+		} catch (NotExistShowException except) {
 			System.out.println(NO_SHOW_SELECTED);
 		}
 	}
+
 	private static void switchToShow(Scanner in, Aplication a1) {
 		String showName = in.nextLine();
 		try {
 			a1.switchToShow(showName);
 			currentShow(a1);
-		}catch (NotExistShowException except) {
+		} catch (NotExistShowException except) {
 			System.out.println(UNKNOWN_SHOW);
 		}
 	}
